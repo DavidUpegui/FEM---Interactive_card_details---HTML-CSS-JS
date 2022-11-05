66f77f6d4422b5d26ac37ac1dddcfd0753c71503
DavidUpegui <david.upegui1@udea.edu.co>
2022-11-05 09:37:26 -0500

    Added the validations for the form.
    Every input have its own HTML verificator (including the confirm
    button). The verification work fine.

--- js/script.js
+++ js/script.js
@@ -12,13 +12,19 @@ const app = (()=>{
     const btnContinue = document.getElementById('btn-continue');
     const form = document.getElementById('form');
     const succes = document.getElementById('succes');
+    const excepName = document.getElementById('excep-name');
+    const excepNumber = document.getElementById('excep-number');
+    const excepMonth = document.getElementById('excep-month');
+    const excepYear = document.getElementById('excep-year');
+    const excepCvc = document.getElementById('excep-cvc');
+    const excepSubmit = document.getElementById('excep-submit');
 
     function init(){
-        inptNumber.txtElement = txtNumber;
-        inptName.txtElement = txtName;
-        inptMonth.txtElement = txtDate;
-        inptYear.txtElement = txtDate;
-        inptCvc.txtElement = txtCvc;
+        inptName.valid = false;
+        inptNumber.valid = false;
+        inptMonth.valid = false;
+        inptYear.valid = false;
+        inptCvc.valid = false;
         initInptNumber(inptNumber);
         initInptName();
         initInptMonth();
@@ -26,9 +32,34 @@ const app = (()=>{
         initInptCvc();
         initBtnConfirm();
         initBtnContinue()
-
     }
     
+    function initInptName(){
+        inptName.addEventListener('keydown', (e)=>{
+            if(e.key.match(/[^A-zÁ-ú'Ä-ü. ]/i)){
+                e.preventDefault();
+            }
+        })
+        inptName.addEventListener('keyup', ()=>{
+            if(inptName.value.length === 0){
+                txtName.innerHTML = 'Jane ApleSeed';
+            }else{
+                txtName.innerHTML = inptName.value;
+            }
+        })
+        inptName.addEventListener('blur', ()=>{
+            if(inptName.value.length < 5){
+                excepName.classList.remove('hide');
+                inptName.classList.add('form__input--invalid');
+                inptName.valid = false;
+            }else{
+                excepName.classList.add('hide');
+                inptName.classList.remove('form__input--invalid');
+                inptName.valid = true;
+            }
+        });
+    }
+
     function initInptNumber(inpt){
         inpt.addEventListener('keydown', (e)=>{
             if(e.key.match(/[^0-9]/i)){
@@ -42,6 +73,29 @@ const app = (()=>{
             formatValue(inpt)
             controlCaretPosition(inpt,e.key);
             writeCardNumber();
+        })
+        inptNumber.addEventListener('change', ()=>{
+            console.log('Cambió')
+            if(inptNumber.value.length !== 19){
+                if(inptNumber.value.length !== 0){
+                    excepNumber.innerHTML = `The Number must contain 16 digits.`
+                }
+                excepNumber.classList.remove('hide');
+            }
+        })
+        inptNumber.addEventListener('blur', ()=>{
+            if(inptNumber.value.length !== 19){
+                if(inptNumber.value.length !== 0){
+                    excepNumber.innerHTML = `The Number must contain 16 digits.`
+                }
+                excepNumber.classList.remove('hide');
+                inptNumber.classList.add('form__input--invalid');
+                inptNumber.valid = false;
+            }else{
+                excepNumber.classList.add('hide');
+                inptNumber.classList.remove('form__input--invalid');
+                inptNumber.valid = true;
+            }
         })
     }
     function formatValue(inpt){
@@ -92,21 +146,10 @@ const app = (()=>{
     function writeCardNumber(){
         let initStr = 'XXXX XXXX XXXX XXXX'
         if (inptNumber.value.length === 0){
-            inptNumber.txtElement.innerHTML = initStr;
+            txtNumber.innerHTML = initStr;
         }else{
-            inptNumber.txtElement.innerHTML = inptNumber.value + initStr.slice(inptNumber.value.length);
+            txtNumber.innerHTML = inptNumber.value + initStr.slice(inptNumber.value.length);
         }
-    }
-
-    function initInptName(){
-        inptName.addEventListener('keydown', (e)=>{
-            if(e.key.match(/[^A-zÁ-ú'Ä-ü. ]/i)){
-                e.preventDefault();
-            }
-        })
-        inptName.addEventListener('keyup', ()=>{
-            txtName.innerHTML = inptName.value;
-        })
     }
 
     function initInptMonth(){
@@ -126,27 +169,24 @@ const app = (()=>{
             }else{
                 txtDate.innerHTML = initMonth.slice(inptMonth.value.length) + inptMonth.value +  '/' + year;
             }
-            if(inptMonth.value > 12){
-                console.log('Error')
-            }
         })
-    }
+        inptMonth.addEventListener('blur', ()=>{
+            if(inptMonth.value.length === 0){
+                excepMonth.classList.remove('hide');
+                inptMonth.classList.add('form__input--invalid');
+                inptMonth.valid = false;
 
-    function initInptCvc(){
-        inptCvc.addEventListener('keydown', (e)=>{
-            if(e.key == ' ' || e.key === '.' || e.key === ',' || e.key === '+' || e.key == '-'){
-                e.preventDefault()
-            }
-            if(inptCvc.value.length > 2 && e.key.match(/[0-9]/)){
-                e.preventDefault();
-            }
-        })
-        inptCvc.addEventListener('keyup', ()=>{
-            let initCvc = '000'
-            if(inptCvc.value.length === 0){
-                txtCvc.innerHTML = initCvc;
             }else{
-                txtCvc.innerHTML = initCvc.slice(inptCvc.value.length) + inptCvc.value
+                if(inptMonth.value > 12){
+                    excepMonth.innerHTML = 'Invalid month.'
+                    excepMonth.classList.remove('hide');
+                    inptMonth.classList.add('form__input--invalid');
+                    inptMonth.valid = false;
+                }else{
+                    excepMonth.classList.add('hide');
+                inptMonth.classList.remove('form__input--invalid');
+                inptMonth.valid = true;
+                }
             }
         })
     }
@@ -168,8 +208,56 @@ const app = (()=>{
             }else{
                 txtDate.innerHTML = month +  '/' + initYear.slice(inptYear.value.length) + inptYear.value;
             }
-            if(inptYear.value > 22 && inptYear.value < 92){
-                console.log('Error')
+        })
+        inptYear.addEventListener('blur', ()=>{
+            if(inptYear.value.length === 0){
+                excepYear.classList.remove('hide');
+                inptYear.classList.add('form__input--invalid');
+                inptYear.valid = false;
+            }else{
+                if(inptYear.value > 22 && inptYear.value < 88){
+                    excepYear.innerHTML = 'Invalid year.'
+                    excepYear.classList.remove('hide');
+                    inptYear.classList.add('form__input--invalid');
+                    inptYear.valid = false;
+                }else{
+                    excepYear.classList.add('hide');
+                    inptYear.classList.remove('form__input--invalid');
+                    inptYear.valid = true;
+                }
+            }
+        })
+    }
+
+    function initInptCvc(){
+        inptCvc.addEventListener('keydown', (e)=>{
+            if(e.key == ' ' || e.key === '.' || e.key === ',' || e.key === '+' || e.key == '-'){
+                e.preventDefault()
+            }
+            if(inptCvc.value.length > 2 && e.key.match(/[0-9]/)){
+                e.preventDefault();
+            }
+        })
+        inptCvc.addEventListener('keyup', ()=>{
+            let initCvc = 'XXX'
+            if(inptCvc.value.length === 0){
+                txtCvc.innerHTML = initCvc;
+            }else{
+                txtCvc.innerHTML = inptCvc.value + initCvc.slice(inptCvc.value.length)
+            }
+        })
+        inptCvc.addEventListener('blur', ()=>{
+            if(inptCvc.value.length !== 3){
+                if(inptCvc.value.length !== 0){
+                    excepCvc.innerHTML = `The CVC must contain 3 digits.`;
+                }
+                excepCvc.classList.remove('hide');
+                inptCvc.classList.add('form__input--invalid');
+                inptCvc.valid = false;
+            }else{
+                excepCvc.classList.add('hide');
+                inptCvc.classList.remove('form__input--invalid');
+                inptCvc.valid = true;
             }
         })
     }
@@ -177,8 +265,14 @@ const app = (()=>{
     function initBtnConfirm(){
         btnConfirm.addEventListener('click', (e)=>{
             e.preventDefault();
-            form.classList.add('hide');
-            succes.classList.remove('hide');
+            let valid = inptCvc.valid && inptMonth.valid && inptYear.valid && inptName.valid && inptNumber.valid;
+            if(valid){
+                form.classList.add('hide');
+                excepSubmit.classList.add('hide');
+                succes.classList.remove('hide')
+            }else{
+                excepSubmit.classList.remove('hide');
+            }
         })
     }
     function initBtnContinue(){
@@ -186,6 +280,15 @@ const app = (()=>{
             e.preventDefault()
             form.classList.remove('hide');
             succes.classList.add('hide');
+            inptName.value = ''
+            txtName.innerHTML = 'Jane ApleSeed';
+            inptNumber.value = ''
+            txtNumber.innerHTML = 'XXXX XXXX XXXX XXXX';
+            inptMonth.value = ''
+            inptYear.value = ''
+            txtDate.innerHTML = '00/00';
+            inptCvc.value = ''
+            txtCvc.innerHTML = 'XXX';
         })
     }
 
